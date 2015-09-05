2e83b3d3c44f6daa12e708ba375d7df42e8f45d6
unknown <schmrj@comcast.net>
2015-09-05 15:27:26 -0400

    Fixed registration request client call to return the Registration Response.

--- antisleuth/AntiSleuthClient/src/main/java/com/antisleuthsecurity/client/auth/Authentication.java
+++ antisleuth/AntiSleuthClient/src/main/java/com/antisleuthsecurity/client/auth/Authentication.java
@@ -3,11 +3,9 @@ package com.antisleuthsecurity.client.auth;
 import javax.ws.rs.core.MediaType;
 
 import com.antisleuthsecurity.asc_api.exceptions.AscException;
-import com.antisleuthsecurity.asc_api.rest.UserAccount;
 import com.antisleuthsecurity.asc_api.rest.requests.RegistrationRequest;
 import com.antisleuthsecurity.asc_api.rest.responses.RegistrationResponse;
 import com.antisleuthsecurity.asc_api.utilities.ASLog;
-import com.antisleuthsecurity.asc_api.utilities.GeneralUtils;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
@@ -15,19 +13,24 @@ public class Authentication {
 
 	ASLog logger = new ASLog("Client");
 
-	public UserAccount registerUser(RegistrationRequest request,
+	/**
+	 * 
+	 * @param {@link RegistrationRequest} containing the information of the user
+	 *        you wish to register
+	 * @param {@link WebResource} for the API Endpoint
+	 * @return {@link RegistrationResponse} containing the results of the
+	 *         registration attempt
+	 * @throws AscException
+	 */
+	public RegistrationResponse registerUser(RegistrationRequest request,
 			WebResource resource) throws AscException {
-
 		try {
 			ClientResponse response = resource.path("/auth/register")
 					.type(MediaType.APPLICATION_JSON)
 					.post(ClientResponse.class, request);
 
 			if (response.getStatus() == 200) {
-				String stringResponse = response.getEntity(String.class);
-//				RegistrationResponse regResponse = response.getEntity(RegistrationResponse.class);
-//				UserAccount account = regResponse.getUserAccount();
-				System.out.println("Received: " + stringResponse);
+				return response.getEntity(RegistrationResponse.class);
 			} else {
 				throw new AscException("Invalid Response: "
 						+ response.getStatus() + " "
@@ -36,7 +39,5 @@ public class Authentication {
 		} catch (Exception e) {
 			throw new AscException("Could not complete request", e);
 		}
-
-		return null;
 	}
 }
